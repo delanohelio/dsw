b1a254eed47a043fdcae5ed5f0217dc34eefa6f0
Delano Oliveira <delanohelio@gmail.com>
2025-06-29 18:10:26 -0300

    Código do miniapp livro_vuejs

--- miniapps/livros_vue/vue.js
+++ miniapps/livros_vue/vue.js
@@ -3,12 +3,40 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            novoTitulo: '',
+            novoAutor: '',
             message: 'Olá, Vue!',
+            idLivro: 2,
             // Nossos livros, tarefas, ou qualquer outro dado virá aqui
             livros: [
-                { id: 1, titulo: 'O Alquimista', autor: 'Paulo Coelho' },
-                { id: 2, titulo: 'Dom Casmurro', autor: 'Machado de Assis' },
+                { id: 0, titulo: 'O Alquimista', autor: 'Paulo Coelho' },
+                { id: 1, titulo: 'Dom Casmurro', autor: 'Machado de Assis' },
             ]
         }
+    },
+
+    methods: {
+        adicionarLivro(e) {
+            e.preventDefault()
+            // 'this' se refere à instância do Vue
+            if (this.novoTitulo.trim() === '') return;
+
+            this.livros.push({
+                id: this.idLivro++,
+                titulo: this.novoTitulo,
+                autor: this.novoAutor
+            });
+
+            // Limpa os campos do formulário
+            this.novoTitulo = '';
+            this.novoAutor = '';
+        },
+        excluirLivro(livro) {
+            const index = this.livros.indexOf(livro);
+            if (index === -1) return;
+            this.livros.splice(index, 1);
+        }
+
     }
 }).mount('#app');
+
